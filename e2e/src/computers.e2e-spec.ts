1eaffff3b11e2359623c8414831945d838c2cb7a
Anthony PFEIFER <anthhony.pfeifer@gmail.com>
2020-02-20 16:50:15 +0100

    Version Finale

--- e2e/src/computers.e2e-spec.ts
+++ e2e/src/computers.e2e-spec.ts
@@ -6,13 +6,12 @@ describe('Testing Subject: Computers', () => {
     let numberLineInit: number;
     beforeEach(() => {
         page = new ComputerPage();
-        numberLineInit = 0;
         browser.get('/home');
     });
 
     it('Testing numbers of lines in the table, then testing if we are in add page ', () => {
         element.all(by.css('.computerLine')).then(totalRows => {
-            numberLineInit = totalRows.length;
+            this.numberLineInit = totalRows.length;
         });
         element.all(by.css('#addSectionBtn')).first().click();
         expect(browser.driver.getCurrentUrl()).toContain('/add');
@@ -20,8 +19,9 @@ describe('Testing Subject: Computers', () => {
     it('Testing to fill form. Then testing if I\'m back on dashboard (accueil) ', () => {
         browser.get('/add');
         page.completeForm();
-        element.all(by.id('submitBtn')).click();
-        expect(browser.driver.getCurrentUrl()).toContain('/home'); // Visiblement, j'ai dû rater le routing. Mais j'arrive pas à y régler
+        element.all(by.css('#submitBtn')).click();
+        page.sleep();
+        expect(browser.driver.getCurrentUrl()).toContain('home');
         page.sleep();
     });
     it('Testing if we have one more line ', () => {
--- e2e/src/computers.po.ts
+++ e2e/src/computers.po.ts
@@ -4,19 +4,19 @@ export class ComputerPage {
     completeForm() {
         let modele = element.all(by.id('modele'));
         let marque = element.all(by.id('inputRadio-Dell'));
-        let type = element.all(by.id('inputRadio-Portable'));
+        let type = element.all(by.name('type'));
         let category = element.all(by.id('inputRadio-Bureautique'));
         let prixAchat = element.all(by.id('prixAchat'));
         let prixVente = element.all(by.id('prixVente'));
         modele.sendKeys('test');
         marque.click();
-        type.click();
+        type.sendKeys('Portable');
         category.click();
         prixAchat.sendKeys(420);
         prixVente.sendKeys(420);
     }
     sleep() {
-        browser.driver.sleep(3000);
+        browser.driver.sleep(4000);
     }
 }
 
